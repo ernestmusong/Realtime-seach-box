e433916b554938b6384a8a1c109cf8528a6bc4b6
Musong <ernestakeh@gmail.com>
2024-01-05 10:26:11 +0100

    Add css and implement method to retrieve saved queries based on user ip address

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -17,13 +17,29 @@ class SearchesController < ApplicationController
     'when are',
     'when should'
   ]
+
+  @topics = [
+    'cook rice',
+    'bake a cake',
+    'learn programming',
+    'start a business',
+    'grow plants',
+    'write a book',
+    'play guitar',
+    'build a website'
+  ]
+
+
+  def index
+    incomplete_query = unsummarize_query(query)
+    @results = perform_search(incomplete_query)
+    render json: { results: @resutls }
+  end
+
   def create
     query = params[:query]
     ip_address = request.remote_ip
-    incomplete_query = unsummarize_query(query)
     complete_query = query if summarize_query(query)
-    @results = perform_search(incomplete_query)
-
     if complete_query.present?
       @search = Search.new(query: complete_query, ip_address: ip_address)
       @search.save
@@ -45,6 +61,21 @@ class SearchesController < ApplicationController
   end
 
   def perform_search(query)
-    Search.where('LOWER(query) LIKE ?', "%#{query.downcase}%").pluck(:query)
+    @user_ip = request.remote_ip
+    saved_queries = Search.where(ip_address: user_ip).pluck(:query)
+
+    suggestions = @prefixes.map do |prefix|
+      topics.map do |topic|
+        "#{prefix} #{topic}"
+      end
+    end.flatten
+
+    # Check if there are any saved queries matching the user input
+    matching_queries = saved_queries.select { |saved_query| saved_query.downcase.include?(query.downcase) }
+
+    # If there are matching queries, return them
+    return matching_queries if matching_queries.present?
+
+    suggestions.select { |suggestion| suggestion.downcase.include?(query.downcase) }
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,8 +1,4 @@
 class Search < ApplicationRecord
   validates :query, presence: true
   validates :ip_address, presence: true
-
-  def self.analytics
-    group(:query).order('COUNT(*) DESC').count
-  end
 end
