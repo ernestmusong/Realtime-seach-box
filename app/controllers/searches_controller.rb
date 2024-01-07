229b02c36f26bf73b7123e980d56964c711bbaf3
Musong <ernestakeh@gmail.com>
2024-01-07 16:15:06 +0100

    Limit last word to 3

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,16 +4,32 @@ class SearchesController < ApplicationController
   def create
     query = params[:query].to_s.strip.downcase
     user_ip = request.remote_ip
+
+    # Retrieve existing search suggestions based on the entered prefix and matching ip_address
     search_suggestions = Search.where('lower(query) LIKE ? AND ip_address = ?', "#{query}%", user_ip)
-      .group(:query, :count).order('count DESC').limit(10).pluck(:query)
-    if query.split.length >= 3
-      existing_search = Search.find_by(query: query)
+                                .group(:query, :count)
+                                .order('count DESC')
+                                .limit(10)
+                                .pluck(:query)
+
+    # Check if the current query is a prefix of any existing search queries
+    existing_search = Search.find_by('lower(query) = ? AND ip_address = ?', query, user_ip)
+
+    if query.split.length >= 3 && last_word_length_not_less_than_3?(query)
       if existing_search
         existing_search.increment!(:count)
       else
         Search.create(query: query, ip_address: user_ip)
       end
     end
+
     render json: { suggestions: search_suggestions }
+  end
+
+  private
+
+  def last_word_length_not_less_than_3?(query)
+    last_word = query.split.last
+    last_word.length >= 3
   end
 end
